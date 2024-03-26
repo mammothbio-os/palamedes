f442eb271fb14254d31dc57a2987483b37daa39f
Kyle McChesney <mbio.kyle@gmail.com>
2024-03-26 09:39:03 -0600

    address some missed PR feedback: more CLI options, use __main__ instead of CLI, remove unused config, etc

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ version = '0.0.0'
 setup(
     name='palamedes',
     version=version,
+    python_requires='>=3.11',
     description='Palamedes: HGVS variants from a sequence alignment',
     author_email='open-source@mammothbiosci.com',
     entry_points={
-        'console_scripts': ['palamedes=palamedes.cli:main'],
+        'console_scripts': ['palamedes=palamedes.__main__:main'],
     },
     packages=find_packages(),
     install_requires=['biopython~=1.83', 'hgvs~=1.5.4'],
