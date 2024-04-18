bb92347c86d4962e3c0ed024a5b7e0c2ccc035b5
Kyle McChesney <mbio.kyle@gmail.com>
2024-04-18 12:24:08 -0600

    Bump version: 0.0.4 → 0.0.5

--- palamedes/__init__.py
+++ palamedes/__init__.py
@@ -8,7 +8,7 @@ from palamedes.hgvs.utils import categorize_variant_block
 from palamedes.hgvs.builders import BUILDER_CONFIG
 
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 
 def generate_hgvs_variants(
